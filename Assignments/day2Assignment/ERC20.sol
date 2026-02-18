908aafa52f3d08f24c668b8d5e9bf255a2d4573e
Feyisara2108 <ayokomi2108@gmail.com>
2026-02-18 13:07:34 +0100

    feat: Added proper modifier for the ERC20 to restrict people that can mint the token

--- Assignments/day2Assignment/ERC20.sol
+++ Assignments/day2Assignment/ERC20.sol
@@ -1,68 +1,90 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.30;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 _amount) external returns (bool);
-    // function allowance() external view returns (uint256);
-    function approve(address spender, uint256 _amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 _amount) external returns (bool);
-
-    event Transfer(address indexed from, address to, uint256 indexed _amount);
-    event Approval(address indexed owner, address spender, uint256 indexed _amount);
-}
-
-abstract contract ERC20 is IERC20 {
-    uint256 public totalSupply; //total coin available mint = +ve, burn + burn
+contract Mutmahinat {
+    uint256 public totalSupply;
 
     mapping(address _owner => uint256 balance) public balanceOf;
+    mapping(address _owner => mapping(address _spender => uint256 _amount)) public allowance;
 
-    mapping(address _owner => mapping(address _spender => uint256 _amount)) allowance;
+    string public constant name = "Mutmahinat";
+    string public constant symbol = "MUT";
+    uint8 public constant decimals = 18;
 
-    string public name = "ERC20";
-    string public symbol = "RBNNT";
-    uint8 public decimal = 18;
+    address public owner;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed _amount);
+    event Approval(address indexed owner, address indexed spender, uint256 indexed _amount);
+
+    error NotOwner(address caller);
+    error ZeroAddress();
+    error InsufficientBalance(uint256 available, uint256 required);
+    error InsufficientAllowance(uint256 available, uint256 required);
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner(msg.sender);
+        _;
+    }
+
+    constructor(uint256 _initialSupply) {
+        owner = msg.sender;
+        _mint(msg.sender, _initialSupply);
+    }
 
     function transfer(address recipient, uint256 _amount) external returns (bool) {
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _amount;
+        if (recipient == address(0)) revert ZeroAddress();
+        if (balanceOf[msg.sender] < _amount) revert InsufficientBalance(balanceOf[msg.sender], _amount);
 
-        balanceOf[recipient] = balanceOf[recipient] + _amount;
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[recipient] += _amount;
 
         emit Transfer(msg.sender, recipient, _amount);
-
         return true;
     }
 
     function approve(address spender, uint256 _amount) external returns (bool) {
+        if (spender == address(0)) revert ZeroAddress();
+
         allowance[msg.sender][spender] = _amount;
 
         emit Approval(msg.sender, spender, _amount);
-
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 _amount) external returns (bool) {
-        allowance[sender][msg.sender] = allowance[sender][msg.sender] - _amount;
+        if (recipient == address(0)) revert ZeroAddress();
+        if (allowance[sender][msg.sender] < _amount) {
+            revert InsufficientAllowance(allowance[sender][msg.sender], _amount);
+        }
+        if (balanceOf[sender] < _amount) revert InsufficientBalance(balanceOf[sender], _amount);
 
-        balanceOf[sender] = balanceOf[sender] - _amount;
-
-        balanceOf[recipient] = balanceOf[recipient] + _amount;
+        allowance[sender][msg.sender] -= _amount;
+        balanceOf[sender] -= _amount;
+        balanceOf[recipient] += _amount;
 
         emit Transfer(sender, recipient, _amount);
-
         return true;
     }
 
-    function mint(uint256 _amount) external {
-        balanceOf[msg.sender] = balanceOf[msg.sender] + _amount;
-        totalSupply = totalSupply + _amount;
-        emit Transfer(address(0), msg.sender, _amount);
+    function mint(uint256 _amount) external onlyOwner {
+        _mint(msg.sender, _amount);
     }
 
     function burn(uint256 _amount) external {
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _amount;
-        totalSupply = totalSupply - _amount;
+        if (balanceOf[msg.sender] < _amount) revert InsufficientBalance(balanceOf[msg.sender], _amount);
+
+        balanceOf[msg.sender] -= _amount;
+        totalSupply -= _amount;
+
         emit Transfer(msg.sender, address(0), _amount);
+    }
+
+    function _mint(address _to, uint256 _amount) internal {
+        if (_to == address(0)) revert ZeroAddress();
+
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+
+        emit Transfer(address(0), _to, _amount);
     }
 }
